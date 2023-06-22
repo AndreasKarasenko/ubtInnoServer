4540fac1b3422c9a2e58554780e884f7e4fd7580
AndreasKarasenko <52670382+AndreasKarasenko@users.noreply.github.com>
2023-06-22 15:46:43 +0200

    Update install.sh

--- installation/install.sh
+++ installation/install.sh
@@ -1,3 +1,6 @@
 sudo apt update && sudo apt upgrade
 sudo apt install neovim
 sudo apt install curl
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
